2f264593a167c918ed8c86584dbcef53fc54d38c
Tiago Filipe Coelho <tiago.coelho@radboudumc.nl>
2025-01-15 12:27:43 +0100

    try install feature

---
+++ budapest/service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "budapest-schedule-v1";
+const urlsToCache = ["/", "/index.html", "/install.html", "/styles.css", "/graphics/apple-touch-icon.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
